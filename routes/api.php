be390729c19f8085a847083d5a7a6f7768b93d17
simpxplay@gmail.com <simpxplay@gmail.com>
2022-01-23 14:21:00 +0200

    Testy

--- routes/api.php
+++ routes/api.php
@@ -14,10 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::post('user/create','UserController@createUser');
-Route::get('user/{id}','UserController@getUser');
-Route::delete('user/delete/{id}','UserController@deleteUser');
-
